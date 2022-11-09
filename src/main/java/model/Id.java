88fa3666bd9bb4f65189630a4cdf874f61aca381
Khojiakbar <hojakbarabiyorov@gmail.com>
2022-11-09 16:11:14 +0500

    OLX_bot, BaseMethod , DataBase

---
+++ src/main/java/model/Id.java
@@ -0,0 +1,12 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Id {
+    private int id;
+}
