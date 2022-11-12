0abb6b2975b8dafe981d2cf885770ebb5a3c0249
Abdulhafiz98 <101938839+Abdulhafiz98@users.noreply.github.com>
2022-11-12 18:05:48 +0500

    Update Olx_botBase.java

--- src/main/java/t_bot/Olx_botBase.java
+++ src/main/java/t_bot/Olx_botBase.java
@@ -1,7 +1,7 @@
 package t_bot;
 
 public interface Olx_botBase {
-    String BOT_TOKEN = "5632876718:AAFUUeaDbNxKTo_56Ji75Yq4DRrDPaDRRqc";
-    String BOT_USERNAME = "https://t.me/olxTelegram_B24_bot";
+    String BOT_TOKEN = "";
+    String BOT_USERNAME = "";
 
 }
